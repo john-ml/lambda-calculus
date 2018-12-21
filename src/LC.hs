5eaa113ae45a704f1d8fb1557ffa850351ddf172
John Li <johnli0135@gmail.com>
2018-12-21 02:13:20 -0500

    vertical alignment

--- src/LC.hs
+++ src/LC.hs
@@ -16,7 +16,6 @@ module LC
   ) where
 
 import Data.Map
-import Data.Function (on)
 
 data Term
   = Lit Int
@@ -27,14 +26,14 @@ data Term
 infixl 6 :$
 
 instance Show Term where
-  show (Lit a) = show a
-  show (f@(Λ _) :$ Lit a) = "(" ++ show f ++ ") " ++ show a
+  show (Lit a)             = show a
+  show (f@(Λ _) :$ Lit a)  = "(" ++ show f ++ ") " ++ show a
   show (f@(Λ _) :$ Hole a) = "(" ++ show f ++ ") " ++ a
-  show (f :$ Lit a) = show f ++ " " ++ show a
-  show (f :$ Hole a) = show f ++ " " ++ a
-  show (f :$ e) = show f ++ " (" ++ show e ++ ")"
-  show (Λ e) = "λ " ++ show e ++ ""
-  show (Hole s) = s
+  show (f :$ Lit a)        = show f ++ " " ++ show a
+  show (f :$ Hole a)       = show f ++ " " ++ a
+  show (f :$ e)            = show f ++ " (" ++ show e ++ ")"
+  show (Λ e)               = "λ " ++ show e ++ ""
+  show (Hole s)            = s
 
 mapExpr
   :: (Int -> Int -> Term)
@@ -42,10 +41,10 @@ mapExpr
   -> Term
   -> Term
 mapExpr f g = go 0 where
-  go n (Lit a) = f n a
-  go n (Hole s) = g n s
+  go n (Lit a)   = f n a
+  go n (Hole s)  = g n s
   go n (f' :$ e) = go n f' :$ go n e
-  go n (Λ e) = Λ (go (n + 1) e)
+  go n (Λ e)     = Λ (go (n + 1) e)
 
 mapLits :: (Int -> Int -> Term) -> Term -> Term
 mapLits f = mapExpr f (const Hole)
@@ -60,14 +59,14 @@ pretty m = show . go 0 where
   go n e@(f :$ e') = try e (go n f :$ go n e')
   go n e@(Λ e') =
     case unchurch e of
-      Just k -> Hole $ "'" ++ show k
+      Just k  -> Hole $ "'" ++ show k
       Nothing -> try e (Λ (go (n + 1) e'))
   go _ e = e
 
   unchurch (Λ (Λ e)) = go' e where
-    go' (Lit 0) = Just 0
+    go' (Lit 0)       = Just 0
     go' (Lit 1 :$ e') = (1 +) <$> go' e'
-    go' _ = Nothing
+    go' _             = Nothing
   unchurch _ = Nothing
 
 adjustFree :: (Int -> Int) -> Term -> Term
@@ -82,21 +81,20 @@ substitute e = mapLits (\ n a ->
 fill :: Map String Term -> Term -> Term
 fill m = mapHoles (\ n s ->
   case m !? s of
-    Just e -> adjustFree (+ n) e
+    Just e  -> adjustFree (+ n) e
     Nothing -> Hole s)
 
 reduce :: Term -> Term
-reduce (Lit a) = Lit a
-reduce (Λ e' :$ e) = substitute e e'
+reduce (Lit a)      = Lit a
+reduce (Λ e' :$ e)  = substitute e e'
 reduce (Lit n :$ e) = Lit n :$ reduce e
-reduce (f :$ e) = reduce f :$ e
-reduce (Λ e) = Λ (reduce e)
-reduce (Hole s) = Hole s
+reduce (f :$ e)     = reduce f :$ e
+reduce (Λ e)        = Λ (reduce e)
+reduce (Hole s)     = Hole s
 
 simplify :: Int -> Term -> Term
 simplify steps e =
-  let (f ||| g) a b = f a b || g a b in
-  let e' = whileNot (((>) `on` sizeof) ||| (==)) reduce e in
+  let e'  = whileNot (\ a' a -> sizeof a' > sizeof a || a' == a) reduce e in
   let e'' = iterate reduce e' !! steps in
   if sizeof e'' < sizeof e' then e'' else e'
 
@@ -104,10 +102,10 @@ evaluate :: Term -> Term
 evaluate = whileNot (==) reduce
 
 sizeof :: Integral a => Term -> a
-sizeof (Lit _) = 1
+sizeof (Lit _)  = 1
 sizeof (Hole _) = 1
 sizeof (f :$ e) = 1 + sizeof f + sizeof e
-sizeof (Λ e) = 1 + sizeof e
+sizeof (Λ e)    = 1 + sizeof e
 
 whileNot :: (a -> a -> Bool) -> (a -> a) -> a -> a
 whileNot p f a =
