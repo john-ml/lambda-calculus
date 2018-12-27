02c58d8264bd88d9a6d9db029dff9ce148ce76d3
John Li <johnli0135@gmail.com>
2018-12-27 13:24:00 -0500

    replace ∀ with λ

--- app/Main.hs
+++ app/Main.hs
@@ -13,24 +13,24 @@ repl = forever $ do
 
 test :: IO ()
 test = do
-  testParse "∀ a: Type 0. ∀ b: Type 0. λ f: (λ _: a. b). λ x: a. f x"
-  testParse "∀ a: Type t, ∀ a: Type t1, ∀ t2: Type t + 1 ∧ t1 + (t ∧ t1), a"
+  testParse "λ a: Type 0. λ b: Type 0. λ f: (λ _: a. b). λ x: a. f x"
+  testParse "λ a: Type t, λ a: Type t1, λ t2: Type t + 1 ∧ t1 + (t ∧ t1), a"
   testExecute $
     "λ t: Type 0, λ t1: Type 0, λ f: Type 0, λ x: Type 0, " ++
     "(λ a: Type 0. λ b: Type 0. λ f: a -> b. λ x: a. f x) t t1 f x"
-  testInfer "∀ A : Type 0, ∀ B : Type 0, λ a : A, λ b : B, a"
-  testInfer "∀ A : Type 0, ∀ B : Type 0, λ f : A -> B, λ x : A, f x"
+  testInfer "λ A : Type 0, λ B : Type 0, λ a : A, λ b : B, a"
+  testInfer "λ A : Type 0, λ B : Type 0, λ f : A -> B, λ x : A, f x"
   testInfer $
-    "∀ A : Type 0, λ f : A -> A, λ x : A, f (f (f (f (f (f (f (f x)))))))"
+    "λ A : Type 0, λ f : A -> A, λ x : A, f (f (f (f (f (f (f (f x)))))))"
   testInfer $
-    "∀ A : Type 0, ∀ B : Type 0, ∀ C : Type 0, " ++
+    "λ A : Type 0, λ B : Type 0, λ C : Type 0, " ++
     "λ f : B -> C, λ g : A -> B, λ x : A, f (g x)"
   testInfer $
-    "∀ A : Type n, ∀ B : Type n, ∀ C : Type n, " ++
+    "λ A : Type n, λ B : Type n, λ C : Type n, " ++
     "λ f : A -> B -> C, λ x : B, λ y : A, f y x"
-  testInfer $ "λ f : (∀ A : Type n, A -> A), ∀ B : Type 0, λ x : B, f B x"
-  testInfer $ "λ f : (∀ A : Type n, A -> A), ∀ B : Type n + 1, λ x : B, f B x"
-  testInfer $ "λ f : (∀ A : Type 1, A -> A), ∀ B : Type 0, λ x : B, f (Type 0) B"
+  testInfer $ "λ f : (λ A : Type n, A -> A), λ B : Type 0, λ x : B, f B x"
+  testInfer $ "λ f : (λ A : Type n, A -> A), λ B : Type n + 1, λ x : B, f B x"
+  testInfer $ "λ f : (λ A : Type 1, A -> A), λ B : Type 0, λ x : B, f (Type 0) B"
 
 tryEither :: Either String b -> (b -> IO ()) -> IO ()
 tryEither e f = flip (either putStrLn) e f
